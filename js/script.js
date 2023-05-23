fda67c21007f2ec56ee6686dddd457c5ee886a75
salvomc <smcrwrk@gmail.com>
2023-05-23 18:25:27 +0200

    milestone 2/3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
 
 
+let images = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
+
+let slider = document.getElementById("slider");
+
+
+for ( let i = 0; i < images.length; i++ ) 
+
+    {
+        slider.innerHTML += `<div class="item"> <img src="${images[i]}" alt=""> </div> `;   
+    }
+
+let items = document.querySelectorAll(".item");
+let count = 0;
+items[count].classList.add("active");
+
+const next = document.querySelector(".button.next");
+const previous = document.querySelector(".button.previous");
+
+
+next.addEventListener("click", function () 
+    
+    {
+        items[count].classList.remove("active");
+        items[count].classList.add("active");     
+    })
+
+previous.addEventListener("click", function () 
+    
+    {
+        items[count].classList.remove("active");
+        items[count].classList.add("active");
+    })
